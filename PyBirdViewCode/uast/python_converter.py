5312c92338cd4499593bfc907897d1c2c51ad71b
hzy14610046011 <1295752786@qq.com>
2024-06-12 16:52:03 +0800

    modified python converter

--- PyBirdViewCode/uast/python_converter.py
+++ PyBirdViewCode/uast/python_converter.py
@@ -18,11 +18,85 @@ class ParsoASTConverter:
             "expr_stmt": self._handle_expr_stmt,
             "yield_expr": self._handle_yield_expr,
             "if_stmt": self._handle_if_stmt,
+            "funcdef": self._handle_funcdef,
+            "suite": self._handle_suite,
+            # "newline": lambda _: (),
+            "simple_stmt": lambda stmt: self.eval_single_node(stmt.children[0]),
+            "while_stmt": self._handle_while,
+            "atom": self._handle_atom,
+            "return_stmt": self._handle_return,
+            "keyword": self._handle_keyword,
         }
 
         # Return value of execution
         self._ret_value = None
         # self._labels: Dict[str, LabelDesc] = {}
+
+    def _handle_keyword(self, c: parso_tree.ReturnStmt) -> nodes.ReturnStmt:
+        if c.value == "return":
+            return nodes.ReturnStmt()
+        else:
+            raise NotImplementedError(c)
+
+    def _handle_return(self, c: parso_tree.Keyword) -> nodes.Stmt:
+        print(c.children)
+        returned_value = c.children[1]
+        match returned_value:
+            case PythonNode(type="testlist_star_expr"):
+
+                return nodes.ReturnStmt(
+                    [
+                        self.eval_single_node(val)
+                        for val in returned_value.children
+                        if val.type not in ("operator",)
+                    ]
+                )
+            case _:
+                return nodes.ReturnStmt([self.eval_single_node(returned_value)])
+
+    def _handle_atom(self, c: parso_tree.PythonNode) -> nodes.Expr:
+        if len(c.children) == 3:
+            match (c.children[0], c.children[2]):
+                case (parso_tree.Operator(value="("), parso_tree.Operator(value=")")):
+                    return self.eval_single_node(c.children[1])
+        raise NotImplementedError
+
+    def _handle_while(self, c: parso_tree.WhileStmt) -> nodes.WhileStmt:
+        print(c, c.children)
+        operator_node = c.children[1]
+        body_node = c.children[3]
+        # raise NotImplementedError
+        return nodes.WhileStmt(
+            self.eval_single_node(operator_node), self.eval_single_node(body_node)
+        )
+
+    def _handle_suite(self, c: parso_tree.PythonNode) -> nodes.BlockStmt:
+
+        return nodes.BlockStmt(
+            [
+                self.eval_single_node(stmt)
+                for stmt in c.children
+                if stmt.type not in ("newline")
+            ]
+        )
+
+    def _handle_funcdef(self, c: parso_tree.Function) -> nodes.MethodDecl:
+        # param: parso_tree.Param
+        # param.
+        method_type = nodes.MethodType(
+            [
+                nodes.ParamDecl(self.eval_single_node(param.name), nodes.UnknownType("any"))
+                for param in c.get_params()
+            ],
+            nodes.UnknownType("any"),
+        )
+        method_decl = nodes.MethodDecl(
+            self.eval_single_node(c.name),
+            method_type,
+            self.eval_single_node(c.children[-1]),
+            # c.iter
+        )
+        return method_decl
 
     def _handle_notimplemented(self, c: NodeOrLeaf) -> nodes.NotImplementedItem:
         return nodes.NotImplementedItem(str(c.kind))
@@ -36,8 +110,12 @@ class ParsoASTConverter:
 
     def _handle_if_stmt(self, node: parso_tree.IfStmt) -> nodes.IfThenElseStmt:
         # print(node.children)
-        _, namedexpr_test, _, suite, *remainings = nodes.children
-        raise NotImplementedError
+        _, namedexpr_test, _, suite, *remainings = node.children
+        stmt = nodes.IfThenElseStmt(
+            self.eval_single_node(namedexpr_test), self.eval_single_node(suite)
+        )
+        assert len(remainings) == 0
+        return stmt
 
     def _handle_expr_stmt(self, node: parso_tree.ExprStmt) -> Union[nodes.Expr]:
         if len(node.children) == 2:
@@ -90,5 +168,6 @@ class ParsoASTConverter:
         try:
             return self._handlers_map[node.type](node)
         except KeyError as e:
-            print(node)
-            raise e
+            print(node, node.type)
+            print(e)
+            raise ValueError
